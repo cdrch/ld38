52a3c7888a41e83096f4c12602506b2c1f3226ad
Carter David Henning <carterdavidhenning@gmail.com>
2017-04-22 02:21:00 -0400

    Added character movement; added testing scene for movement and camera

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float walkSpeed = 5f;
+    public float jumpSpeed = 7f;
+    public float verticalLookSpeed = 10f;
+    public float rotationInterpolationSpeed = 0.15f;
+
+    Rigidbody rb;
+    Collider col;
+
+    private bool pressedJump = false;
+
+	void Start ()
+	{
+        rb = GetComponent<Rigidbody>();
+        col = GetComponent<Collider>();
+	}
+
+	void Update ()
+	{
+        HandleWalk();
+        HandleJump();
+	}
+
+    private void HandleWalk()
+    {
+        // Reset the velocity to zero
+        rb.velocity = new Vector3(0f, rb.velocity.y, 0f);
+
+        // The amount that should be moved in this frame
+        float distance = walkSpeed * Time.deltaTime;
+
+        float hAxis = Input.GetAxis("Horizontal");
+        float vAxis = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(hAxis * distance, 0f, vAxis * distance);
+
+        if (movement.x != 0 || movement.z != 0)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), rotationInterpolationSpeed);
+        }        
+
+        Vector3 currPosition = transform.position;
+        Vector3 newPosition = currPosition + movement;
+
+        rb.MovePosition(newPosition);
+    }
+
+    private void HandleJump()
+    {
+        float jAxis = Input.GetAxis("Jump");
+        bool isGrounded = CheckGrounded();
+
+        if (jAxis > 0f)
+        {
+            if (!pressedJump && isGrounded)
+            {
+                pressedJump = true;
+                Vector3 jumpVector = new Vector3(0f, jumpSpeed, 0f);
+
+                rb.velocity = rb.velocity + jumpVector;
+            }
+        }
+        else
+        {
+            pressedJump = false;
+        }
+    }
+
+    private bool CheckGrounded()
+    {
+        float sizeX = col.bounds.size.x;
+        float sizeZ = col.bounds.size.z;
+        float sizeY = col.bounds.size.y;
+
+        // Position of the 4 bottom corners of the game object
+        // We add 0.01 in Y as a skin so that there is some distance between the point and the floor
+        Vector3 corner1 = transform.position + new Vector3(sizeX / 2, -sizeY / 2 + 0.01f, sizeZ / 2);
+        Vector3 corner2 = transform.position + new Vector3(-sizeX / 2, -sizeY / 2 + 0.01f, sizeZ / 2);
+        Vector3 corner3 = transform.position + new Vector3(sizeX / 2, -sizeY / 2 + 0.01f, -sizeZ / 2);
+        Vector3 corner4 = transform.position + new Vector3(-sizeX / 2, -sizeY / 2 + 0.01f, -sizeZ / 2);
+
+        // Send a short ray to detect ground
+        bool grounded1 = Physics.Raycast(corner1, new Vector3(0, -1, 0), 0.01f);
+        bool grounded2 = Physics.Raycast(corner2, new Vector3(0, -1, 0), 0.01f);
+        bool grounded3 = Physics.Raycast(corner3, new Vector3(0, -1, 0), 0.01f);
+        bool grounded4 = Physics.Raycast(corner4, new Vector3(0, -1, 0), 0.01f);
+
+        // If any corner is grounded, the object is grounded
+        return (grounded1 || grounded2 || grounded3 || grounded4);
+    }
+}
---
+++ Assets/Scripts/Unused And Outdated Scripts For Reference/PlayerMovement.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float speed = 0f;
+    public float maxSpeed = 10f;
+    public float acceleration = 10f;
+    public float deceleration = 10f;
+
+    public float jumpHeight = 5f; // This is how high you want the character to jump
+    public float timeToJumpApex = 2f; // This is how long you want it to take for the max jump height to be achieved
+    private float gravity; // Based on jumpHeight and timeToJumpApex
+    private float jumpVelocity; // Based on jumpHeight and timeToJumpApex
+
+    public float jumpCooldown = 0.5f;
+    private float timeToNextJump;
+    public int maxJumps = 1;
+    private int jumpsRemaining;
+
+    private Vector3 movement;
+
+    private Rigidbody rb;
+
+    private Vector3 prevVelocity;
+    private Vector3 targetVelocity;
+
+	void Start ()
+	{
+        rb = GetComponent<Rigidbody>();
+        movement = Vector3.zero;
+
+        // Calculate jump information based on desired variables - see: https://www.youtube.com/watch?v=PlT44xr0iW0&index=3&list=PLFt_AvWsXl0f0hqURlhyIoAabKPgRsqjz
+        gravity = -(2 * jumpHeight) / Mathf.Pow(timeToJumpApex, 2);
+        jumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
+        Debug.Log("Gravity: " + gravity + " Jump Velocity: " + jumpVelocity);
+
+        jumpsRemaining = maxJumps;
+        timeToNextJump = 0f;
+	}
+
+	void Update ()
+	{
+        /*
+        prevVelocity = rb.velocity;
+        targetVelocity = new Vector3(Input.GetAxis("Horizontal") * speed, 0f, Input.GetAxis("Vertical") * speed);
+
+        rb.velocity = Vector3.Lerp(prevVelocity, targetVelocity, 0.1f);
+        
+
+        if (Input.GetAxis("Horizontal") != 0 && speed < maxSpeed)
+        {
+            speed = speed - acceleration * Time.deltaTime;
+        }*/
+	}
+
+    private void FixedUpdate()
+    {
+        Move(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")); // information drawn from here: http://answers.unity3d.com/questions/29751/gradually-moving-an-object-up-to-speed-rather-then.html
+    }
+
+    private void Move(float h, float v)
+    {
+        if (v != 0f || h != 0f)
+        {
+            movement.Set(h, 0f, v);
+            speed = Mathf.Min(speed + acceleration * Time.deltaTime, maxSpeed);
+        }
+        else
+        {
+            speed = Mathf.Max(speed - deceleration * Time.deltaTime, 0f);
+        }
+        //Debug.Log(speed);
+
+        // Normalize the movement vector, then make it proportional to the speed per second
+        movement = movement.normalized * speed * Time.deltaTime;
+
+        //movement.Set(movement.x, Input.GetButtonDown, movement.z)
+        // Move the player to current position + movement
+        rb.MovePosition(transform.position + movement);
+
+        //Debug.Log(Input.GetButtonDown("Jump"));
+        if (Input.GetButtonDown("Jump") && jumpsRemaining > 0 && timeToNextJump == 0f)
+        {
+            //Debug.Log("Should be jumping");
+            rb.AddForce(0f, jumpVelocity, 0f, ForceMode.VelocityChange);
+            jumpsRemaining--;
+            timeToNextJump = jumpCooldown;
+        }
+        else
+        {
+            timeToNextJump -= Time.deltaTime;
+            timeToNextJump = Mathf.Max(timeToNextJump, 0f);
+        }
+        
+    }
+
+    private void Jump(float j)
+    {
+
+    }
+
+    public void SetReadyToJump()
+    {
+        jumpsRemaining = maxJumps;
+    }
+}
