58e8a277b28953b20cb04588e9c235e6750db521
Holden Profit <chobits00_01@yahoo.com>
2017-04-22 13:36:02 -0400

    Added basis for Inventory system, re-organized scripts a bit.

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour {
+    public Texture2D m_GUITexture;
+
+	// Use this for initialization
+	void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
