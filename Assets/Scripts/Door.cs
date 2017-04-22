442b97f35029e3ca003c1391659144af28427acd
Holden Profit <chobits00_01@yahoo.com>
2017-04-22 18:50:34 -0400

    Updated door script.

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -5,58 +5,29 @@ using UnityEngine;
 public class Door : Interactable
 {
     public bool m_isOpen = false;
-    private bool m_isMoving = false;
-
-    public float m_closedRotation; // 0
-    public float m_openedRotation; // 1
-    private float m_rotationTime;
 
 	// Use this for initialization
 	void Start ()
     {
-        if (m_isOpen)
-        {
-            m_openedRotation = this.gameObject.transform.localRotation.z;
-        }
-        else
-        {
-            m_closedRotation = this.gameObject.transform.localRotation.z;
-        }
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        
-		if (m_isMoving)
-        {
-            m_rotationTime += (m_isOpen ? -1 : 1) * Time.deltaTime;
-            if (m_rotationTime > 1f || m_rotationTime < 0f)
-            {
-                m_isMoving = false;
-            }
-            float currentRotationAngle = Mathf.Lerp(m_closedRotation, m_openedRotation, m_rotationTime);
-            Vector3 currentRotation = this.gameObject.transform.localRotation.eulerAngles;
-            float deltaAngle = currentRotationAngle - currentRotation.z;
-            //this.gameObject.transform.localRotation.Set(currentRotation.x, currentRotation.y, currentRotationAngle, currentRotation.w);
-            this.gameObject.transform.Rotate(Vector3.up * (deltaAngle * Mathf.Deg2Rad));
-        }
-        
-
-
 	}
 
     public override bool Interact(PlayerController interactor)
     {
-        if (!m_isMoving)
+        Debug.Log("Opening Door");
+        if (m_isOpen)
         {
-            Debug.Log("Opening Door");
-            m_isOpen = !m_isOpen;
-            //m_rotationTime = m_isOpen ? 1f : 0f;
-            this.GetComponent<Animator>().Play("Open");
-            m_isMoving = true;
-            return true;
+            this.GetComponent<Animator>().Play("Close");
         }
-        return false;
+        else
+        {
+            this.GetComponent<Animator>().Play("Open");
+        }
+        m_isOpen = !m_isOpen;
+        return true;
     }
 }
