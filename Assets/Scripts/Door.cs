9372c8af1c90ea42b1a554827dc0a69421dae7d8
Carter David Henning <carterdavidhenning@gmail.com>
2017-04-23 19:07:52 -0400

    Added auto-closing doors.

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -6,9 +6,14 @@ public class Door : Interactable
 {
     public bool m_isOpen = false;
 
+    private float timeUntilDoorClose;
+    private Collider m_collider;
+
 	// Use this for initialization
 	void Start ()
     {
+        timeUntilDoorClose = 5f;
+        m_collider = GetComponentInChildren<Collider>();
 	}
 	
 	// Update is called once per frame
@@ -26,8 +31,18 @@ public class Door : Interactable
         else
         {
             this.GetComponent<Animator>().Play("Open");
+            StartCoroutine(CloseDoorAfterWait(timeUntilDoorClose));
         }
         m_isOpen = !m_isOpen;
         return true;
+    }
+
+    private IEnumerator CloseDoorAfterWait(float secondsToWait)
+    {
+        m_collider.enabled = false;
+        yield return new WaitForSeconds(secondsToWait);
+        this.GetComponent<Animator>().Play("Close");
+        m_isOpen = false;
+        m_collider.enabled = true;
     }
 }
